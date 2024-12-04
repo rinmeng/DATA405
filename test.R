e9cd5cb0866d63d34542d8fe7a94b0f45b9a9d12
rin! <phurinmeng@gmail.com>
2024-12-04 00:46:23 -0800

    a4

--- test.R
+++ test.R
@@ -16,22 +16,23 @@ Y <- 21 + 5*X + rnorm(100,  sd= 2.3)
 # ---------------------------------------------  5
 
 # Observed values of X1 and X2
-X1 <- 0.12
-X2 <- 0.5
+X1 <- 1.5
+X2 <- 3.4
+X3 <- 0.20
 
-lambda1 <- 1
-lambda2 <- 10
+lambda1 <- 2
+lambda2 <- 3
 
 # Possible values for lambda
 lambda_values <- c(lambda1, lambda2)
 
 # Calculate the likelihood function for each lambda
-likelihood <- function(lambda, X1, X2) {
-  lambda^2 * exp(-lambda * (X1 + X2))
+likelihood <- function(lambda, X1, X2, X3) {
+  (X1 + X2 + X3)^(lamda - 1) * exp(-x/2)
 }
 
 # Compute likelihoods for each lambda
-likelihoods <- sapply(lambda_values, likelihood, X1 = X1, X2 = X2)
+likelihoods <- sapply(lambda_values, likelihood, X1 = X1, X2 = X2, X3 = X3)
 
 # Display the likelihoods
 likelihoods
@@ -44,9 +45,9 @@ lambda_mle
 # --------------------------------------------- 8
 
 # Given values
-sample_mean <- 17
-population_sd <- 3
-sample_size <- 25
+sample_mean <- 16
+population_sd <- 32
+sample_size <- 16
 confidence_level <- 0.95
 
 # Calculate the standard error
@@ -72,13 +73,13 @@ cat("95% Confidence Interval: (", lower_bound, ", ", upper_bound, ")\n")
 n <- 1000000
 
 # Generate random uniform numbers in the range [-5, 10]
-x_samples <- runif(n, min = -5, max = 10)
+x_samples <- runif(n, min = 0, max = 1)
 
 # Evaluate the function f(x) = e^sin(x) at each random sample
-f_values <- exp(sin(x_samples))
+f_values <- exp(cos(x_samples))
 
 # Estimate the integral: Average of f(x) times the width of the integration interval (15)
-integral_estimate <- mean(f_values) * 15
+integral_estimate <- mean(f_values) * 1
 
 # Standard deviation of the estimates
 std_dev <- sd(f_values)
@@ -100,11 +101,39 @@ cat("95% Confidence Interval: (", lower_bound, ", ", upper_bound, ")\n")
 
 
 # Define the function f(x) = exp(sin(x))
-f <- function(x) exp(sin(x))
+f <- function(x) exp(cos(-1*x))
 
 # Compute the integral numerically using R's integrate function
 true_value <- integrate(f, lower = -5, upper = 10)
 
 # Display the true value of the integral
 cat("True value of the integral:", true_value$value, "\n")
+
+
+#-------------
+glm(formula = y ~ x, family = binomial, data = p13.2)
+
+
+
+
+# a) Standard deviation of the chi-square distribution on 32 degrees of freedom
+chi_square_data <- rchisq(1000000, df=32)
+chi_square_std_dev <- sd(chi_square_data)
+
+# b) Standard deviation of the t distribution on 4 degrees of freedom
+t_data <- rt(1000000, df=4)
+t_std_dev <- sd(t_data)
+
+# c) Mean of the F distribution on 7 numerator and 5 denominator degrees of freedom
+f_data <- rf(1000000, df1=7, df2=5)
+f_mean <- mean(f_data)
+
+# d) Standard deviation of the F distribution on 7 numerator and 5 denominator degrees of freedom
+f_std_dev <- sd(f_data)
+
+# Print results
+round(chi_square_std_dev, 2)
+round(t_std_dev, 2)
+round(f_mean, 2)
+round(f_std_dev, 2)
 
